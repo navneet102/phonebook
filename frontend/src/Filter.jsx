06726f867f68084807dc207ac8127d9fc9a4d3aa
Navneet <conavneet@gmail.com>
2026-06-23 09:23:21 +0000

    Remove frontend folder

--- frontend/src/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = (props) => {
-    return (
-      <div>
-        filter shown with <input value={props.searchName} onChange={props.handleSearchNameChange}/>
-      </div>
-    )
-}
-
-export default Filter
